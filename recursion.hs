0f2e168bd51870d36073e13e036b672d421a1817
Gurpreet Singh <Guppst3r@gmail.com>
2016-08-31 13:54:07 -0400

    more recursion

--- recursion.hs
+++ recursion.hs
@@ -17,3 +17,21 @@ take' n _
     | n <= 0   = []  
 take' _ []     = []  
 take' n (x:xs) = x : take' (n-1) xs  
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [x] -> [y] -> [(x,y)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq x) => x -> [y] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
